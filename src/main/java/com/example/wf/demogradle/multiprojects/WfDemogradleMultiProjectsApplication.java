dda6511c431ce9d97b9fde878a7cef374ebcdf9d
Prashanth Sriram <prashanthsriram145@gmail.com>
2022-07-02 14:45:32 +0530

    Committing gradle artifacts for multiple projects

---
+++ src/main/java/com/example/wf/demogradle/multiprojects/WfDemogradleMultiProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.wf.demogradle.multiprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfDemogradleMultiProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfDemogradleMultiProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wf/demogradle/multiprojects/WfDemogradleMultiProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wf.demogradle.multiprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfDemogradleMultiProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
